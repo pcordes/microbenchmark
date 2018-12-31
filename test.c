b36014f9987000b66f16cd42a77f06eafb8ebbb2
H.J. Lu <hjl.tools@gmail.com>
2018-12-31 08:55:27 -0800

    Support variable loop count

--- test.c
+++ test.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <x86intrin.h>
 
 extern int foo (int);
 extern int foo_no_table (int);
 
-int global = 20;
+int global;
 
 #define LOOP 30000
 
 int
-main ()
+main (int argc, char **argv)
 {
-  int i;
+  int i, loop, total;
   unsigned long long start, end;
   unsigned long long diff1, diff2;
 
+  loop = 0;
+  if (argc > 1)
+    loop = atoi (argv[1]);
+  if (loop <= 0)
+    loop = LOOP;
+
+  printf ("%d loops:\n", loop);
+
+  global = 20;
+  total = 0;
   start = __rdtscp (&i);
-  for (i = 0; i < LOOP; i++)
-    foo_no_table (i);
+  for (i = 0; i < loop; i++)
+    total += foo_no_table (i);
   end = __rdtscp (&i);
   diff1 = end - start;
 
+  printf ("global: %d, total: %d\n", global, total);
   printf ("no jump table: %lld\n", diff1);
 
+  global = 20;
+  total = 0;
   start = __rdtscp (&i);
-  for (i = 0; i < LOOP; i++)
-    foo (i);
+  for (i = 0; i < loop; i++)
+    total += foo (i);
   end = __rdtscp (&i);
   diff2 = end - start;
 
+  printf ("global: %d, total: %d\n", global, total);
   printf ("jump table   : %lld (%.2f%%)\n",
 	  diff2, 100.0f * diff2 / diff1);
 
